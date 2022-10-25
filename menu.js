aa9d3338d6185fe7353a7e3d189b5696b56278d5
DSCHAMALDINOW Mansur <dschamaldinowmansur@MacBook-Air-von-DSCHAMALDINOW.local>
2022-10-25 13:34:33 +0200

    man kann nun mit quit das spiel beenden

--- menu.js
+++ menu.js
@@ -1,22 +1,31 @@
+
+const prompt = require("prompt-sync")()
 module.exports = {
     getMenuOption: function() {
-        /*
-        Should print a menu with the following options:
-        1. Human vs Human
-        2. Random AI vs Random AI
-        3. Human vs Random AI
-        4. Human vs Unbeatable AI
-        The function should return a number between 1-4.
-        If the user will enter invalid data (for example 5), than a message will appear
-        asking to input a new value.
-        */
+        const choices = ["1","2","3","4","5"];
+        let startGame = false;
+        let userInput = '';
+        console.log(`1. Human vs Human\n2. Random AI vs Random AI\n3. Human vs Random AI\n4. Human vs Unbeatable AI\n5. Exit`)
+        userInput = prompt();
+        
+        while(!choices.includes(userInput)){
+            console.log(`you choose : ${userInput}`);
+            console.log("Please choose from the following menu:")
+            console.log(`1. Human vs Human\n2. Random AI vs Random AI\n3. Human vs Random AI\n4. Human vs Unbeatable AI\n5. Exit`)
+
+        
+        userInput = prompt();
+
+        }
+
+        return userInput;
     }
+    
  }
 
 // run this function to test whether you have correctly implemented the other function
-function checkOptions()
-{
-    let option = getMenuOption();
+function checkOptions() {
+    let option = module.exports.getMenuOption();
     console.log("If the user selected 1, it should print 1");
     console.log(option);
 }
