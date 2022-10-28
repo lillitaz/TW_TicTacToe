ef674f7805e5d2a976c95c41a1878f283c7459e6
Silent-Trail <ed.klurfeld@gmail.com>
2022-10-28 13:19:47 +0200

    corrections

--- ticTacToe.js
+++ ticTacToe.js
@@ -1,8 +1,11 @@
 const prompt = require("prompt-sync")();
 
-const board = require ('./board')
 
-let playerVSplayer = true;
+
+let startMenu = true;
+let playerVSplayer = false;
+let playerVSKI = false;
+let playerVSmasterKI = false;
 
 let player1 = 'x';
 let player2 = 'o';
@@ -22,19 +25,66 @@ let winCombi = [
     [gameBoardPattern[2],gameBoardPattern[4],gameBoardPattern[6]],
 ];
 
+//Menu
+while (startMenu===true){
+console.log(`<<<<TIC TAC TOE>>>>\n
+1. Player vs. Player\n
+2. Player vs. KI\n
+3. Player vs. Master KI\n
+4. Quit`);
+
+let startGame = prompt('Choose your Game:');
+
+switch (startGame){
+    case '1': 
+    playerVSplayer = true;
+    startMenu=false;
+    break;
+
+    case '2': 
+    playerVSKI = true;
+    startMenu=false;
+    break;
+
+    case '3': 
+    playerVSmasterKI = true;
+    startMenu=false;
+    break;
+
+    case '4': 
+    console.log('Maybe next time :-(');
+    startMenu=false;
+    break;
+
+    default:
+    console.log('Please chooose 1-5');
+    break;
+}
+}
 
 //Spielstart
 console.log('WELCOME TO TIC TAC TOE');
-board.showEmptyBoard();
+
 console.log(`Player ${currentPlayer} - You start the Game`);
 
 //Spiel
-while (playerVSplayer===true){
+while (playerVSplayer===true){  
     let putStone = prompt('Choose your field:');
     if (gameBoardCoord.includes(putStone)){
-        let correctAnswerIndex = gameBoardCoord.indexOf(putStone);
-        gameBoardPattern.splice(correctAnswerIndex, 1, currentPlayer);
-        gameBoardCoord.splice(correctAnswerIndex, 1, currentPlayer);
+        let answerIndex = gameBoardCoord.indexOf(putStone);
+        gameBoardPattern.splice(answerIndex, 1, currentPlayer);
+        gameBoardCoord.splice(answerIndex, 1, currentPlayer);
+        checkWin();
+        console.log(gameBoardPattern);
+        console.log(checkWin());
+        if (checkWin===true){
+            console.log('WINNER');
+            playerVSplayer = false;
+        }
+        if (putStone.toLowerCase()==='quit'){
+            playerVSplayer = false;
+            startMenu = true;
+        }
         if (currentPlayer===player1){
         currentPlayer = player2;
         }else{
@@ -53,20 +103,22 @@ B | ${gameBoardPattern[3]} | ${gameBoardPattern[4]} | ${gameBoardPattern[5]} |
 C | ${gameBoardPattern[6]} | ${gameBoardPattern[7]} | ${gameBoardPattern[8]} | 
   -------------`);
 console.log(`Player ${currentPlayer}, now is your turn`);
-/*
+
+}
+
 //Siegbedingungen
 function checkWin(){
     for (combi of winCombi){
-        combi.every(function (gameBoardPattern){
-            gameBoardPattern.contains(currentPlayer);
+        const gameWin = combi.every(function(pattern){
+          return pattern.includes(currentPlayer);
         })
+        
+        if (gameWin === true){
+            return true;
+        }
     }
-
+return false;
 }
-*/
-
-}
-
 
 /*
 while(playerVSplayer===true){
